4b2e8c133954145c01466ecba4ab60d3d94ec83f
simchawaintrob <s0527113940@gmail.com>
2019-05-01 12:45:37 +0300

    implement of fibonacci number series
    implement of fibonacci number series in c#

---
+++ FibonacciNumber/Fibonacci.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciNumber
+{
+    class Fibonacci
+    {
+        public double CalculateFibonacci(int index)
+        {
+            double a = 0, b = 1, c = 0;
+
+            if (index == 0)
+                return a;
+            if (index == 1)
+                return b;
+
+
+            for (int i = 2; i <= index; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            return c;
+        }
+
+        public double CalculateFibonacciRecursive(int index)
+        {
+            if (index == 0)
+                return 0;
+            if (index == 1)
+                return 1;
+
+            return CalculateFibonacciRecursive(index - 1) + CalculateFibonacciRecursive(index - 2);
+        }
+    }
+}
---
+++ FibonacciNumber/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+namespace FibonacciNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fib = new Fibonacci();
+
+            Console.WriteLine("get length of fibonacci series");
+            Stopwatch watch = Stopwatch.StartNew();
+
+            var length = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("[{0}] ",fib.CalculateFibonacci(i));
+            }
+            watch.Stop();
+            Console.WriteLine();
+            Console.WriteLine("Done. Took {0} seconds", (double)watch.ElapsedMilliseconds / 1000.0);
+            Console.WriteLine();
+            Console.WriteLine("recursively..");
+
+            watch = Stopwatch.StartNew();
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("[{0}] ", fib.CalculateFibonacciRecursive(i));
+            }
+            watch.Stop();
+            Console.WriteLine();
+
+            Console.WriteLine("Done. Took {0} seconds", (double)watch.ElapsedMilliseconds / 1000.0);
+
+            Console.ReadKey();
+        }
+    }
+}
